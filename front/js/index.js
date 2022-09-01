21376d576383cdf121a3e05e67862da34fe9c450
ManuLFE <emmanuel.lafosse@gmail.com>
2022-09-02 01:31:38 +0200

    replaced loop with "i" with "apiProduct"

--- front/js/index.js
+++ front/js/index.js
@@ -4,30 +4,32 @@
     const value = await result.json();
     const parent = document.getElementById("items");
 
-        for (let i=0; i < value.length; i++) {
+    for (let apiProduct of value) {
 
-            const newAnchorLink = document.createElement("a");
-            newAnchorLink.href = `../html/product.html?id=${value[i]._id}`;
-            parent.appendChild(newAnchorLink);
+        const newAnchorLink = document.createElement("a");
+        newAnchorLink.href = `../html/product.html?id=${apiProduct._id}`;
+        parent.appendChild(newAnchorLink);
 
-            const newArticle = document.createElement('article');
+        const newArticle = document.createElement('article');
 
-            const newImage = document.createElement('img');
-            newImage.src = `/back/images/kanap0${i+1}.jpeg`;
-            newImage.alt = `${value[i].altTxt}`;
+        const newImage = document.createElement('img');
+        newImage.src = apiProduct.imageUrl;
+        newImage.alt = apiProduct.altTxt;
 
-            const newTitle = document.createElement("h3");
-            newTitle.innerText = value[i].name;
-            newTitle.classList.add('productName');
+        const newTitle = document.createElement("h3");
+        newTitle.innerText = apiProduct.name;
+        newTitle.classList.add('productName');
 
-            const newText = document.createElement("p");
-            newText.innerText = value[i].description;
-            newText.classList.add('productDescription');
+        const newText = document.createElement("p");
+        newText.innerText = apiProduct.description;
+        newText.classList.add('productDescription');
 
-            newAnchorLink.appendChild(newArticle)
-            newArticle.appendChild(newImage)
-            newArticle.appendChild(newTitle)
-            newArticle.appendChild(newText)
-        }
+        newAnchorLink.appendChild(newArticle)
+        newArticle.appendChild(newImage)
+        newArticle.appendChild(newTitle)
+        newArticle.appendChild(newText)
+
+    }
+
 
 })();
